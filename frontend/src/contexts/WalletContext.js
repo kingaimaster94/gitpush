b19315d83ec6fda4974d2ec2ee2efb233106bac1
kingaimaster94 <kingaimaster94@gmail.com>
2025-02-04 15:38:40 +0000

    add wallet provider

--- frontend/src/contexts/WalletContext.js
+++ frontend/src/contexts/WalletContext.js
@@ -1,37 +1,41 @@
 "use client"
 
 import React, { FC, ReactNode, useMemo } from 'react';
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+import { WagmiProvider } from "wagmi";
+import { omax } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const projectId = "704b7193f8f27869e0a1782d7fdf9d21";
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [omax];
+
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+});
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+});
 
 export const WalletContextProvider = ({
   children,
 }) => {
-  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
-
-  // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new Coin98WalletAdapter(),
-      new LedgerWalletAdapter(),
-      new NekoWalletAdapter(),
-      new SkyWalletAdapter(),
-      new TokenPocketWalletAdapter(),
-    ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [network]
-  );
-
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          {children}
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
   );
 };
 
