ad049690c298423aa86f17f3b2eb6cecb367e3f6
kingaimaster94 <kingaimaster94@gmail.com>
2025-02-09 01:11:24 +0000

    Automated commit to https://github.com/kingaimaster94/git_push.git

---
+++ git_push/git_push.js
@@ -0,0 +1,19 @@
+const simpleGit = require('simple-git');
+
+async function pushToRepo(repoUrl, branch = 'main') {
+    const git = simpleGit();
+    
+    try {
+        await git.add('.');
+        await git.commit(`Automated commit to ${repoUrl}`);
+        await git.remote(['set-url', 'origin', repoUrl]); // Set the target repo
+        await git.push('origin', branch);
+
+        console.log(`Changes pushed to ${repoUrl} on branch ${branch}`);
+    } catch (error) {
+        console.error(`Error pushing to ${repoUrl}:`, error);
+    }
+}
+
+// Example: Push to a specific GitHub repository
+pushToRepo('https://github.com/kingaimaster94/git_push.git', 'main');
